fc7d6f99afe9c19c8743c9dce948c2baa2064903
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-04-25 15:18:31 +0530

    Basic setup of the microservices done

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,14 +1,46 @@
 import express from 'express';
-
+import { SignUpSchema, SignInSchema } from '@repo/common/types';
 const app = express();
 
-const port = 3001;
+app.use(express.json());
+const port = 3005;
 
 app.post("/signup", (req, res) => {
-
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+    if (!username || !password || !name) {
+        res.status(400).json({
+            message: "Please provide all the fields"
+        })
+        return;
+    }
+    const correctSignUpBody = SignUpSchema.safeParse(req.body);
+    if (!correctSignUpBody.success) {
+        const errorMessage = correctSignUpBody.error.errors.map((error) => error.message)
+        res.status(411).json({
+            message: errorMessage
+        })
+        return;
+    }
 });
 
 app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        res.status(400).json({
+            message: "Please provide all required fields"
+        })
+    }
+    const correctSignInBody = SignInSchema.safeParse(req.body)
+    if (!correctSignInBody.success) {
+        const errorMessage = correctSignInBody.error.errors.map((error) => error.message)
+        res.status(411).json({
+            message: errorMessage
+        })
+        return;
+    }
 
 })
 
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,8 +4,14 @@ const wss = new WebSocketServer({
     port : 8080 
 })
 
+wss.on('listening',() => {
+    console.log(`WebSocket Server is running on port ${wss.options.port}`)
+})
+
 wss.on('connection', (ws) => {
+    console.log('Client connected')
     ws.on('message', (msg) => {
+        console.log(msg)
         ws.send('pong')
     })
 })
