a17cec509b126ec2be05253f2060591480bcb0d1
soumyarikbanerjee <126083869+banerjeesoumya@users.noreply.github.com>
2025-05-01 01:49:53 +0530

    Added the signup/signin routes

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,16 +1,20 @@
 import express from 'express';
 import { SignUpSchema, SignInSchema } from '@repo/common/types';
+import jwt from 'jsonwebtoken';
 import { middleware } from './middleware';
+import { prismaClient } from '@repo/db/client'
+import { JWT_SECRET } from '@repo/backend-common/config';
+
 const app = express();
 
 app.use(express.json());
 const port = 3005;
 
-app.post("/signup", (req, res) => {
-    const username = req.body.username;
+app.post("/signup", async (req, res) => {
+    const email = req.body.username;
     const password = req.body.password;
     const name = req.body.name;
-    if (!username || !password || !name) {
+    if (!email || !password || !name) {
         res.status(400).json({
             message: "Please provide all the fields"
         })
@@ -24,9 +28,44 @@ app.post("/signup", (req, res) => {
         })
         return;
     }
+    try {
+        const userExists = await prismaClient.user.findUnique({
+            where: {
+                email: correctSignUpBody.data.email
+            }
+        })
+        if (userExists) {
+            res.status(400).json({
+                message: "User already exists"
+            })
+        } else {
+            const user = await prismaClient.user.create({
+                data: {
+                    email: correctSignUpBody.data.email,
+                    password: correctSignUpBody.data.password,
+                    name: correctSignUpBody.data.name
+                }
+            })
+
+            res.status(200).json({
+                message: "User signup successfull",
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name
+                }
+            })
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: "Internal server error"
+        })
+        return;
+    }
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (!username || !password) {
@@ -39,6 +78,38 @@ app.post("/signin", (req, res) => {
         const errorMessage = correctSignInBody.error.errors.map((error) => error.message)
         res.status(411).json({
             message: errorMessage
+        })
+        return;
+    }
+    try {
+        const user = await prismaClient.findUnique({
+            where:{
+                email: correctSignInBody.data.email,
+                password: correctSignInBody.data.password
+            }
+        })
+        if (!user) {
+            res.status(400).json({
+                message: "Invalid credentials"
+            })
+            return; 
+        }
+        const token = await jwt.sign({
+            id: user.id
+        }, JWT_SECRET)
+        res.status(200).json({
+            message: "User signin successfull",
+            token: token,
+            user: {
+                email: user.email,
+                name: user.name
+            }
+        })
+        return;
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: "Internal server error"
         })
         return;
     }
